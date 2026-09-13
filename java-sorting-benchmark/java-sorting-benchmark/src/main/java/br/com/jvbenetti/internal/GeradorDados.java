3bd62588989a63ff8863d0eca848482639958403
JaoVicy <filipimjoa@gmail.com>
2026-09-13 10:14:49 -0300

    feat: creating GeradorDados.java

---
+++ java-sorting-benchmark/java-sorting-benchmark/src/main/java/br/com/jvbenetti/internal/GeradorDados.java
@@ -0,0 +1,40 @@
+package main.java.br.com.jvbenetti.internal;
+
+import java.util.Random;
+import java.util.Arrays;
+
+public class GeradorDados {
+    private static final Random random = new Random();
+
+    public static int[] gerarAleatorio(int tamanho) {
+        int[] vetor = new int[tamanho];
+        for (int i = 0; i < tamanho; i++) {
+            vetor[i] = random.nextInt(tamanho * 10);
+        }
+        return vetor;
+    }
+
+    public static int[] gerarOrdenado(int tamanho) {
+        int[] vetor = gerarAleatorio(tamanho);
+        Arrays.sort(vetor);
+        return vetor;
+    }
+
+    public static int[] gerarInversamenteOrdenado(int tamanho) {
+        int[] vetor = gerarOrdenado(tamanho);
+        int[] invertido = new int[tamanho];
+        for (int i = 0; i < tamanho; i++) {
+            invertido[i] = vetor[tamanho - 1 - i];
+        }
+        return invertido;
+    }
+
+    public static int[] gerarComDuplicatas(int tamanho) {
+        int[] vetor = new int[tamanho];
+        // Preenche com apenas 5 valores diferentes para forçar muita repetição
+        for (int i = 0; i < tamanho; i++) {
+            vetor[i] = random.nextInt(5); 
+        }
+        return vetor;
+    }
+}
