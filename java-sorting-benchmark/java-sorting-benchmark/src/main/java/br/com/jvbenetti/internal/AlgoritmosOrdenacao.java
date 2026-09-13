a568adebf76d59b7dee8f621ba04b60bd08c49c4
JaoVicy <filipimjoa@gmail.com>
2026-09-13 10:13:17 -0300

    feat: creating AlgoritmosBusca.java

---
+++ java-sorting-benchmark/java-sorting-benchmark/src/main/java/br/com/jvbenetti/internal/AlgoritmosOrdenacao.java
@@ -0,0 +1,75 @@
+package main.java.br.com.jvbenetti.internal;
+
+public class AlgoritmosOrdenacao {
+
+    public static void bubbleSort(int[] vetor) {
+        int n = vetor.length;
+        boolean trocou;
+        for (int i = 0; i < n - 1; i++) {
+            trocou = false;
+            for (int j = 0; j < n - 1 - i; j++) {
+                if (vetor[j] > vetor[j + 1]) {
+                    int temp = vetor[j];
+                    vetor[j] = vetor[j + 1];
+                    vetor[j + 1] = temp;
+                    trocou = true;
+                }
+            }
+            // Se nenhuma troca foi feita na passagem, o vetor já está ordenado (Flag/Sinalizador)
+            if (!trocou) break;
+        }
+    }
+
+    public static void selectionSort(int[] vetor) {
+        int n = vetor.length;
+        for (int i = 0; i < n - 1; i++) {
+            int minIdx = i;
+            for (int j = i + 1; j < n; j++) {
+                if (vetor[j] < vetor[minIdx]) {
+                    minIdx = j;
+                }
+            }
+            int temp = vetor[minIdx];
+            vetor[minIdx] = vetor[i];
+            vetor[i] = temp;
+        }
+    }
+
+    public static void insertionSort(int[] vetor) {
+        int n = vetor.length;
+        for (int i = 1; i < n; ++i) {
+            int chave = vetor[i];
+            int j = i - 1;
+            while (j >= 0 && vetor[j] > chave) {
+                vetor[j + 1] = vetor[j];
+                j = j - 1;
+            }
+            vetor[j + 1] = chave;
+        }
+    }
+
+    public static void quickSort(int[] vetor, int inicio, int fim) {
+        if (inicio < fim) {
+            int indiceParticao = particionar(vetor, inicio, fim);
+            quickSort(vetor, inicio, indiceParticao - 1);
+            quickSort(vetor, indiceParticao + 1, fim);
+        }
+    }
+
+    private static int particionar(int[] vetor, int inicio, int fim) {
+        int pivo = vetor[fim];
+        int i = (inicio - 1);
+        for (int j = inicio; j < fim; j++) {
+            if (vetor[j] <= pivo) {
+                i++;
+                int temp = vetor[i];
+                vetor[i] = vetor[j];
+                vetor[j] = temp;
+            }
+        }
+        int temp = vetor[i + 1];
+        vetor[i + 1] = vetor[fim];
+        vetor[fim] = temp;
+        return i + 1;
+    }
+}
