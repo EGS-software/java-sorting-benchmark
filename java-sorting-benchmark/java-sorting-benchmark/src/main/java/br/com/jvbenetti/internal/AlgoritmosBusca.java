556a7568edb301d214ef5b0fcd98e3769a89e866
JaoVicy <filipimjoa@gmail.com>
2026-09-13 10:12:14 -0300

    feat: creating AlgoritmosOrdenacao.java

---
+++ java-sorting-benchmark/java-sorting-benchmark/src/main/java/br/com/jvbenetti/internal/AlgoritmosBusca.java
@@ -0,0 +1,25 @@
+package br.com.jvbenetti.internal;
+
+public class AlgoritmosBusca {
+    public static int buscaLinear(int[] vetor, int alvo) {
+        for (int i = 0; i < vetor.length; i++) {
+            if (vetor[i] == alvo) {
+                return i;
+            }
+        }
+        return -1;
+    }
+
+    public static int buscaBinaria(int[] vetor, int alvo) {
+        int inicio = 0;
+        int fim = vetor.length - 1;
+
+        while (inicio <= fim) {
+            int meio = inicio + (fim - inicio) / 2;
+            if (vetor[meio] == alvo) return meio;
+            if (vetor[meio] < alvo) inicio = meio + 1;
+            else fim = meio - 1;
+        }
+        return -1;
+    }
+}
